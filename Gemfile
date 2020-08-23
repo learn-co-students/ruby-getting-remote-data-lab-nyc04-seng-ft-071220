86f432a4442d4a16bf642cf8d21b6a49afab60fe
nisayana <nimaevasb@gmail.com>
2020-08-23 17:05:53 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "https://rubygems.org"
 
 gem 'rspec'
 gem 'pry'
+require 'net/http'
+require 'open-uri'
+require 'json'
--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
-# Write your code here
+class GetRequester
+
+    attr_accessor :url
+    
+    def initialize (url)
+        @url = "http://data.cityofnewyork.us/resource/uvks-tn5n.json" 
+    end
+
+    def get_response_body
+        get_qeuester = GetRequester.new(url)
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        programs = JSON.parse(self.get_response_body)
+    end
+
+end
+
+
+# programs = GetPrograms.new.get_programs
+# puts programs
